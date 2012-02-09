6f06e5fcb62551e1072e5c59cf5832d0dcc4a564
Motiur Rahman <motiur_rahman@hotmail.com>
2012-02-09 21:51:12 +0600

    requires further implementation

---
+++ 2012/CSC373_Algorithm/improvedcountingsort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc , char*argv)
+{
+
+	/*Need to include -ve numbers
+	Main idea , stable sorting 
+	-ve numbers can be made positive
+	insert into a new array in the
+	opposite direction*/
+
+	printf("Needs to be implemented");
+
+	return 0;
+
+}
+
